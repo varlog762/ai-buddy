4d1610aa33e32a0f99ed774b74e34da181bd0589
Gregory Pereverzev <boot762@gmail.com>
2025-02-15 14:58:25 +0300

    feat: update chats-schema migration

--- src/constants/default-messages.js
+++ src/constants/default-messages.js
@@ -3,16 +3,20 @@ export const STARTING_MESSAGE = `*Welcome to AI Chat Bot!* 🤖
 This is an advanced AI-powered chat bot for Telegram, designed for intelligent and natural conversations with multiple Large Language Models (LLMs).  
 
 **Key Features:**  
+
 ✅ AI-driven responses for engaging interactions  
+
 ✅ Supports multiple LLMs:  
-   - 🦙 *Meta Llama 3.1* (Default) - \`meta-llama/llama-3.1-70b-instruct:free\`  
-   - 🔍 *DeepSeek* - \`deepseek/deepseek-r1:free\`  
-   - 🌟 *Google Gemini* - \`google/gemini-2.0-flash-lite-preview-02-05:free\`  
+   - 🦙 *Meta Llama 3.1* (Default) - \`llama-3.1-70b\`  
+   - 🔍 *DeepSeek* - \`deepseek-r1\`  
+   - 🌟 *Google Gemini* - \`gemini-2.0-flash-lite-preview-02-05\`  
+
 ✅ Easily switch models anytime using \`/change-model\`  
+
 ✅ Chat history management  
 
 *By default, Meta Llama 3.1 is selected.* However, you can switch models now or later using the \`/change-model\` command.  
 
-⚠️ *If a model is temporarily unavailable due to high demand, consider switching to another or waiting for access to be restored.*  
+⚠️ *If a model is temporarily unavailable due to high demand (you may receive errors), consider switching to another or waiting for access to be restored.*  
 
 Would you like to change the model now or keep the default one?`;
