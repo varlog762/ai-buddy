3231d25c312f082e260f481611503dcaa8828dcc
Gregory Pereverzev <boot762@gmail.com>
2025-02-15 20:29:00 +0300

    feat: add CALLBACK_QUERY constant to events.js

--- src/constants/events.js
+++ src/constants/events.js
@@ -1,4 +1,5 @@
 const MESSAGE = 'message';
+const CALLBACK_QUERY = 'callback_query';
 const MESSAGE_FROM_TG = 'message-from-tg';
 const MESSAGE_FROM_AI = 'message-from-ai';
 const CLEAR_CHAT_HISTORY = 'clear-chat-history';
@@ -8,6 +9,7 @@ export {
   MESSAGE_FROM_TG,
   MESSAGE_FROM_AI,
   MESSAGE,
+  CALLBACK_QUERY,
   CLEAR_CHAT_HISTORY,
   LLM_SELECTED,
 };
