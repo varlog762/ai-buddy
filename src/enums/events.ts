23d94bd72cc477cd6e14a2385f8922ecb9683a61
Gregory Pereverzev <boot762@gmail.com>
2025-03-18 15:11:59 +0300

    feat: add Events enum

---
+++ src/enums/events.ts
@@ -0,0 +1,10 @@
+export enum Events {
+  MESSAGE = 'message',
+  CALLBACK_QUERY = 'callback_query',
+  MESSAGE_FROM_TG = 'message-from-tg',
+  VOICE_MESSAGE_FROM_TG = 'voice-message-from-tg',
+  MESSAGE_TO_TG = 'message-to-tg',
+  CLEAR_CHAT_HISTORY = 'clear-chat-history',
+  LLM_SELECTED = 'llm-selected',
+  SHOW_CURRENT_LLM = 'show-current-llm',
+}
--- src/enums/index.ts
+++ src/enums/index.ts
@@ -2,3 +2,4 @@ export * from './ai-models';
 export * from './chat-commands';
 export * from './chat-roles';
 export * from './error-messages';
+export * from './events';
