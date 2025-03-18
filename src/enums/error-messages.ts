3ed754cc13f88761d051cfba44b442a0d489af99
Gregory Pereverzev <boot762@gmail.com>
2025-03-18 15:09:06 +0300

    feat: add ErrorMessages enum

---
+++ src/enums/error-messages.ts
@@ -0,0 +1,10 @@
+export enum ErrorMessages {
+  SOMETHING_WRONG = 'Oops! Something went wrong. Try again later.',
+  DATABASE_SAVING = 'Error while saving the message:',
+  CHAT_DATA = 'Error while getting the chat data:',
+  GET_LLM_NAME = 'Error while getting the LLM name:',
+  GET_SYSTEM_MESSAGE = 'Error while getting the system message:',
+  FALSY_LLM_NAME = 'Model cannot be null or undefined',
+  UPDATE_MODEL = 'Error while updating the model:',
+  DELETE_CHAT_HISTORY = 'Error while deleting the chat history:',
+}
--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,3 +1,4 @@
 export * from './ai-models';
 export * from './chat-commands';
 export * from './chat-roles';
+export * from './error-messages';
