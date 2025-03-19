0b36b761e43180b31d0728eaa6f6808fb010ceac
Gregory Pereverzev <boot762@gmail.com>
2025-03-19 14:15:53 +0300

    feat: add telegram-file.ts

--- src/utils/telegram-file.ts
+++ src/utils/telegram-file.ts
@@ -3,14 +3,16 @@ import { convertBlobToBuffer } from './index.js';
 
 const { TELEGRAM_API_TOKEN: token, TELEGRAM_API_URL: baseUrl } = process.env;
 
-const getFilePath = async voiceMessageFileId => {
+const getFilePath = async (voiceMessageFileId: string): Promise<string> => {
   const url = `${baseUrl}bot${token}/getFile?file_id=${voiceMessageFileId}`;
   const response = await fetch(url);
   const fileInfo = await response.json();
   return fileInfo.result?.file_path;
 };
 
-export const getVoiceMessageFromTelegram = async voiceMessageFileId => {
+export const getVoiceMessageFromTelegram = async (
+  voiceMessageFileId: string
+): Promise<Blob> => {
   const filePath = await getFilePath(voiceMessageFileId);
   const url = `${baseUrl}file/bot${token}/${filePath}`;
   const response = await fetch(url);
@@ -18,7 +20,9 @@ export const getVoiceMessageFromTelegram = async voiceMessageFileId => {
   return blob;
 };
 
-export const getBufferFromTelegramVoiceMessage = async voiceMessageFileId => {
+export const getBufferFromTelegramVoiceMessage = async (
+  voiceMessageFileId: string
+): Promise<Buffer> => {
   try {
     const blob = await getVoiceMessageFromTelegram(voiceMessageFileId);
     return await convertBlobToBuffer(blob);
